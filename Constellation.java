7f6a7bf3ac2cf1c785e28b58c0721c0ca64a0a4b
gl-aarav <gl.aarav@gmail.com>
2025-02-26 13:35:15 -0800

    Updates

--- Constellation.java
+++ Constellation.java
@@ -3,22 +3,22 @@
  * Aarav Goyal
  * Constellation.java
  */
- import java.awt.Color;
- import java.awt.Graphics;
- import java.awt.Font;
- import javax.swing.JFrame;
- import javax.swing.JPanel;
- 
- public class Constellation
- {
-	 public static void main (String [] args)
-	 {
-		 Constellation c = new Constellation();
-		 c.runIt();
-	 }
-	 
-	 public void runIt()
-	 {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Constellation
+{
+	public static void main (String [] args)
+	{
+		Constellation c = new Constellation();
+		c.runIt();
+	}
+
+	public void runIt()
+	{
 		JFrame frame = new JFrame("The Big Dipper");
 		frame.setSize(630, 450);
 		frame.setLocation(100, 50);
@@ -27,8 +27,8 @@
 		DipperPanel mpPanel = new DipperPanel();
 		frame.getContentPane().add(mpPanel);
 		frame.setVisible(true);
-	 }
- }
+	}
+}
 
 class DipperPanel extends JPanel
 {
@@ -46,7 +46,7 @@ class DipperPanel extends JPanel
 		drawPlanet(g);
 		drawRocket(g);
 	}
-	
+
 	public void drawText(Graphics g)
 	{
 		g.setColor(Color.WHITE);
@@ -54,7 +54,7 @@ class DipperPanel extends JPanel
 		g.drawString("The Big Dipper", 170, 80);
 		g.drawRect(160, 40, 270, 65);
 	}
-	
+
 	public void drawMoon(Graphics g)
 	{
 		g.setColor(new Color(255, 255, 0));
@@ -62,22 +62,44 @@ class DipperPanel extends JPanel
 		g.setColor(Color.BLUE);
 		g.fillOval(-20, -20, 110, 110);
 	}
-	
+
 	public void drawDipper(Graphics g)
 	{
 		g.setColor(Color.WHITE);
-		int[] X = {40, 180, 220, 380, 380, 520, 540, 400};
-		int[] Y = {220, 190, 200, 220};
-		g.drawPolygon(X,Y,3);
+		int[] X = {40, 180, 220, 380, 520, 540, 400, 380, 220, 180, 40};
+		int[] Y = {220, 190, 200, 210, 140, 240, 300, 210, 200, 190, 220};
+		g.drawPolygon(X,Y,11);
+
+		g.fillOval(36,216, 8, 8);
+		g.fillOval(175,186, 8, 8);
+		g.fillOval(216,196, 8, 8);
+		g.fillOval(376,206, 8, 8);
+		g.fillOval(516,136, 8, 8);
+		g.fillOval(536,236, 8, 8);
+		g.fillOval(396,296, 8, 8);
 	}
-	
-	public void drawPlanet(Graphics g)
+
+	public void drawPlanet(Graphics g) 
 	{
-		
+		g.setColor(new Color(139, 69, 19));
+		g.fillArc(-75, 370, 800, 200, 0, 180);
 	}
-	
-	public void drawRocket(Graphics g)
+
+	public void drawRocket(Graphics g) 
 	{
-		
+		g.setColor(Color.RED);
+		int[] rocketX = {60, 110, 140, 120, 75};
+		int[] rocketY = {345, 335, 345, 370, 380};
+		g.drawPolygon(rocketX, rocketY, 5);
+
+		int[] flameX1 = {35, 60, 40};
+		int[] flameY1 = {360, 355, 370};
+		g.fillPolygon(flameX1, flameY1, 3);
+
+		int[] flameX2 = {45, 70, 50};
+		int[] flameY2 = {380, 375, 390};
+		g.fillPolygon(flameX2, flameY2, 3);
+
+		g.drawArc(-410, 345, 1100, 130, -90, 180);
 	}
 }
